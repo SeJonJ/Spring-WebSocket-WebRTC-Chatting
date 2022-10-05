28f794d5e4f13b1805f248ef2b2f4605d7855124
wkdtpwhs <wkdtpwhs@gmail.com>
2022-10-05 17:23:46 +0900

    JPA 시작

--- src/main/java/webChat/Entity/Member.java
+++ src/main/java/webChat/Entity/Member.java
@@ -1,2 +1,18 @@
-package webChat.Entity;public class Member {
+package webChat.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    String nickName;
+
+    String email;
+
+    String provider;
 }
