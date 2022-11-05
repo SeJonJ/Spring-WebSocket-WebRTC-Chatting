cf88ca1f43a4943e35225abb3643c00148d1cc8f
SeJonJ <wkdtpwhs@gmail.com>
2022-11-05 18:16:09 +0900

    화면 공유 완료&디버깅중

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ## /./build/libs/Chat-0.0.1-SNAPSHOT.jar  위치에 만들어진 파일을 chatting.jar 로 복사하여 활용
-COPY ./build/libs/Chat-7.5-SNAPSHOT.jar chatting.jar
+COPY ./build/libs/Chat-7.7-SNAPSHOT.jar chatting.jar
 
 ## application.properties 포함
 #ADD build/resources/main/application.properties /app/application.properties
--- src/main/resources/static/js/rtc/webrtc_client.js
+++ src/main/resources/static/js/rtc/webrtc_client.js
@@ -418,7 +418,25 @@ function handleNewICECandidateMessage(message) {
     myPeerConnection.addIceCandidate(candidate).catch(handleErrorMessage);
 }
 
-/*  화면 공유 */
+/** 화면 공유 실행 과정
+ * 나와 연결된 다른 peer 에 나의 화면을 공유하기 위해서는 다른 peer 에 보내는 Track 에서 stream 을 교체할 필요가 있다.
+ * Track 이란 현재 MediaStream 을 구성하는 각 요소를 의미한다.
+ *    - Track 는 오디오, 비디오, 자막 총 3개의 stream 으로 구성된다.
+ *    - 때문에 Track 객체는 track[0] = 오디오, track[1] = 비디오 의 배열 구조로 되어있다
+ * MediaStream 이란 video stream 과 audio steam 등의 미디어 스트림을 다루는 객체를 이야기한다 - 쉽게 생각하자면 비디오와 오디오 데이터 -
+ *
+ * 즉 상대방에게 보내는 track 에서 나의 웹캠 videoStream 대신 공유 화면에 해당하는 videoStream 으로 변경하는 것이다.
+ *
+ * 이렇듯 Track 에서 steam 을 교체 - replace - 하기 위해서는 아래와 같은 과정을 거친다.
+ * 1. myPeerConnection 에서 sender 를 가져온다. sender 란 나와 연결된 다른 peer 로 생각하면 된다.
+ * 2. sender 객체에서 replaceTrack 함수를 활용해서 stream 을 교체한다.
+ * 3. 이때 shareView 의 경우 VideoStream 의 경우 audio 까지 포함해서 화면 공유를 시작한 것이 아니라면
+ *      Track[0] 에 videoStream 이 들어있다. 때문에 replaceTrack 의 파라미터에 shareView.getTrack[0] 을 넣는다.
+ * 4. 화면 공유 취소 시 원래 화상 화면으로 되돌리기 위해서는 다시 Track 를 localstream 으로 교체해주면 된다!
+ *      이때 localStream 에는 audio 와 video 모두 들어가 있음으로 video 에 해당하는 Track[1] 만 꺼내서 교체해준다.
+ * **/
+
+/*  화면 공유를 위한 변수 선언 */
 const screenHandler = new ScreenHandler();
 let shareView = null;
 
@@ -437,7 +455,7 @@ function ScreenHandler() {
         video: {
             width: 1980, // 최대 너비
             height: 1080, // 최대 높이
-            frameRate: 40, // 최대 프레임
+            frameRate: 50, // 최대 프레임
         },
     };
 
@@ -479,8 +497,8 @@ function ScreenHandler() {
             track.stop();
         });
 
-        // 전송 중단 시 share-video 부분 hide
-        $("#share-video").hide();
+        // // 전송 중단 시 share-video 부분 hide
+        // $("#share-video").hide();
     }
 
     /**
@@ -492,44 +510,42 @@ function ScreenHandler() {
 }
 
 /**
- * 로컬 스트림 핸들링
- * @param stream
- */
-// function onLocalStream(stream) {
-//     log('onLocalStream', stream);
-//
-//     const $video = document.querySelector('#share-video');
-//     $video.srcObject = stream;
-// }
-
-/**
- * screenHandler를 통해 스크린 API를 호출합니다 - 시작
+ * screenHandler를 통해 스크린 API를 호출합니다
+ * 원격 화면을 화면 공유 화면으로 교체
  */
 async function startScreenShare() {
-    const stream = await screenHandler.start();
 
-    // 전송 시작 시 share-video 부분 show
-    $("#share-video").show();
+    // 스크린 API 호출 & 시작
+    await screenHandler.start();
 
-    // onLocalStream(stream);
+    // myPeerConnection 에 연결된 다른 sender 쪽으로 - 즉 다른 Peer 쪽으로 -
+    // shareView 의 Track 에서 0번째 인덱스에 들어있는 값 - 즉 videoStream 로 - 교체한다.
+    await myPeerConnection.getSenders().forEach((sender)=>{ // 연결된 sender 로 보내기위한 반복문
 
-    await myPeerConnection.forEach((pc) =>{
-        const sender = pc.getSenders().find((s) =>
-        s.track.kind == shareView.getTracks().kind);
-        sender.replaceTrack(shareView.getTracks());
+        // track 를 shareView 트랙으로 교체
+        sender.replaceTrack(shareView.getTracks()[0])
+
+    })
+
+
+    /**
+     * 화면 공유 중지 눌렀을 때 이벤트
+     * TODO share-video 가 사라지는 것 뿐만 아니라 추가로 video-off 에서 video-on 으로 변경되도록 만들어야함
+     */
+    shareView.getVideoTracks()[0].addEventListener('ended', () =>{
+        // log('screensharing has ended')
+
+        // Track 를 localstream 의 videoStram 로 교체함
+        myPeerConnection.getSenders().forEach((sender) =>{
+            sender.replaceTrack(localStream.getTracks()[1]);
+        })
+
+        // $("#share-video").hide();
+
     });
 
-
-    // /**
-    //  * 화면 공유 중지 눌렀을 때 이벤트
-    //  * share-video 가 사라지는 것 뿐만 아니라 추가로 video-off 에서 video-on 으로 변경되도록 만들어야함
-    //  */
-    // shareView.getVideoTracks()[0].addEventListener('ended', () =>{
-    //     log('screensharing has ended')
-    //
-    //     $("#share-video").hide();
-    //
-    // });
+    // console.dir(shareView.getTracks());
+    // console.dir(localStream.getTracks());
 
 }
 
@@ -537,5 +553,12 @@ async function startScreenShare() {
 *  video off 버튼을 통해 스크린 API 종료
 * */
 async function stopScreenShare(){
+
+    // screen share 종료
     await screenHandler.end();
+    // myPeerConnection
+    await myPeerConnection.getSenders().forEach((sender) =>{
+        sender.replaceTrack(localStream.getTracks()[1]);
+    })
+
 }
