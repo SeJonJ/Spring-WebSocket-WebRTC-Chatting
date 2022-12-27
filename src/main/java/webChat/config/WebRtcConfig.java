d1bd7ef464d682fbc735a15add0165e4e3b5410f
SeJonJ <wkdtpwhs@gmail.com>
2022-12-28 00:38:58 +0900

    webrtc Config 수정

--- src/main/java/webChat/config/WebRtcConfig.java
+++ src/main/java/webChat/config/WebRtcConfig.java
@@ -17,10 +17,11 @@ public class WebRtcConfig implements WebSocketConfigurer {
     /* TODO WebRTC 관련 */
     private final SignalHandler signalHandler;
 
-//    @Bean
-//    public KurentoClient kurentoClient() {
-//        return KurentoClient.create();
-//    }
+    // Kurento Media Server 를 사용하기 위한 Bean 설정
+    @Bean
+    public KurentoClient kurentoClient() {
+        return KurentoClient.create();
+    }
 
     // signal 로 요청이 왔을 때 아래의 WebSockerHandler 가 동작하도록 registry 에 설정
     // 요청은 클라이언트 접속, close, 메시지 발송 등에 대해 특정 메서드를 호출한다
