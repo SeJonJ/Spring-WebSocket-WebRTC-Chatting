5ae4cd0bad355b16b9bb35cf920030f81119f7fe
sejon <wkdtpwhs@gmail.com>
2023-02-01 20:43:30 +0900

    [develop] trying motion tracking...2

--- src/main/resources/static/js/motion/motion.js
+++ src/main/resources/static/js/motion/motion.js
@@ -1,41 +1,166 @@
-async function startWebcam() {
-    const video = document.getElementById('video');
-    const canvas = document.getElementById('video-canvas');
-    const context = canvas.getContext('2d');
+// Pointer finger to draw or pick color; flat "stop" hand to move pointer.  If it's having trouble tracking your finger, either change your background or try including your middle finger with your pointer finger.
+//Handpose code by the ml5.js team.  Visit https://ml5js.org/
+// Drawing code by Steve's Makerspace
+// Video: https://youtu.be/96sWFP9CCkQ
 
-    // Load the handtrack model
-    const model = await handTrack.load();
+let handpose;
+let video;
+let predictions = [];
+let canvas2;
+let prevtop = null;
+let prevleft = null;
+let leftArr = [];
+let topArr = [];
+let leftAvg, topAvg;
+let colr = 0;
+let colb = 255;
+let colg = 0;
+let pointerX, pointerY, knuckle, ring;
 
-    // Access the webcam and start prediction
-    const webcam = await handTrack.startVideo(video);
+function setup() {
+    createCanvas(640, 480);
+    canvas2 = createGraphics(width, height);
+    makesquares();
+    video = createCapture(VIDEO);
+    video.size(width, height);
 
-    let isDrawing = false;
-    let x, y;
+    handpose = ml5.handpose(video, modelReady);
 
-    while (true) {
-        const predictions = await model.detect(video);
+    // This sets up an event that fills the global variable "predictions"
+    // with an array every time new hand poses are detected
+    handpose.on("predict", (results) => {
+        predictions = results;
+    });
 
-        if (predictions.length > 0 && predictions[0].bbox[2] > 50) {
-            // If hand is detected and its width is larger than 50
-            const handBox = predictions[0].bbox;
-            x = handBox[0] + handBox[2] / 2;
-            y = handBox[1] + handBox[3] / 2;
+    // Hide the video element, and just show the canvas
+    video.hide();
+}
 
-            // Start drawing if hand is detected for the first time
-            if (!isDrawing) {
-                context.beginPath();
-                context.moveTo(x, y);
-                isDrawing = true;
-            } else {
-                context.lineTo(x, y);
-                context.stroke();
+function modelReady() {
+    console.log("Model ready!");
+}
+
+function draw() {
+    translate(width, 0);
+    scale(-1, 1);
+    //  background(0);
+
+    image(video, 0, 0, width, height);
+    image(canvas2, 0, 0);
+
+    // We can call both functions to draw all keypoints and the skeletons
+    drawKeypoints();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+    for (let i = 0; i < predictions.length; i += 1) {
+        const prediction = predictions[i];
+        canvas2.strokeWeight(10);
+        for (let j = 0; j < prediction.landmarks.length; j += 1) {
+            const keypoint = prediction.landmarks[j];
+            fill(0, 255, 0);
+            noStroke();
+            //   ellipse(keypoint[0], keypoint[1], 10, 10);
+            if (j == 8) {
+                pointerX = keypoint[0];
+                pointerY = keypoint[1];
+                //print(keypoint);
+            } else
+            if (j == 14) {
+                knuckle = keypoint[1];
+            } else
+            if (j == 16) {
+                ring = keypoint[1];
             }
-        } else if (isDrawing) {
-            // Stop drawing if hand is not detected
-            isDrawing = false;
         }
+        //If the ring finger is not extended then draw a line or pick a color
+        if (knuckle < ring) {
+            fill(0);
+            ellipse(pointerX, pointerY, 10, 10);
+            if (pointerX < width - 70) {
+                getaverages();
 
+                canvas2.stroke(colr, colg, colb);
+                if (leftArr.length > 2 && prevleft>0) {
+                    canvas2.line(prevleft, prevtop, leftAvg, topAvg);
+                    if (prevleft > 0) {
+                        prevleft = leftAvg;
+                        prevtop = topAvg;}
+                    else{
+                        prevleft = pointerX;
+                        prevtop = pointerY;
+                    }
+                }
+            } else {
+                if (pointerY < 70) {
+                    colr = 255;
+                    colg = 0;
+                    colb = 0;
+                }
+
+                if (pointerY > 70 && pointerY < 140) {
+                    colr = 0;
+                    colg = 255;
+                    colb = 0;
+                }
+                if (pointerY > 140 && pointerY < 210) {
+                    colr = 0;
+                    colg = 0;
+                    colb = 255;
+                }
+                if (pointerY > 210 && pointerY < 280) {
+                    makesquares();
+                }
+            }
+        } else {
+            //If the hand is extended, then just mark where it is and clear the arrays
+            fill(255);
+            ellipse(pointerX, pointerY, 10, 10);
+            leftArr.length = 0;
+            topArr.length = 0;
+            leftAvg = 0;
+            topAvg = 0;
+            prevleft = pointerX;
+            prevtop = pointerY;
+        }
     }
 }
 
-startWebcam();
+function getaverages() {
+    if (leftArr.length > 5) {
+        leftArr.splice(0, 1);
+        topArr.splice(0, 1);
+    }
+    if (pointerX > 0 ) {
+        leftArr.push(pointerX);
+        topArr.push(pointerY);
+    }
+    let leftSum = 0;
+    let topSum = 0;
+    for (i = 0; i < leftArr.length; i++) {
+        leftSum = leftSum + leftArr[i];
+        topSum = topSum + topArr[i];
+    }
+    leftAvg = leftSum / leftArr.length;
+    topAvg = topSum / topArr.length;
+
+}
+
+function makesquares() {
+    canvas2.background(255);
+    canvas2.clear();
+    //background(255);
+    //clear();
+    canvas2.fill(255, 0, 0);
+    canvas2.rect(width, 0, -70, 70);
+    canvas2.fill(0, 255, 0);
+    canvas2.rect(width, 70, -70, 70);
+    canvas2.fill(0, 0, 255);
+    canvas2.rect(width, 140, -70, 70);
+    canvas2.fill(0, 0, 0);
+    canvas2.rect(width, 210, -70, 70);
+    canvas2.stroke(255, 0, 0);
+    canvas2.strokeWeight(10);
+    canvas2.line(width - 5, 215, width - 65, 275);
+}
