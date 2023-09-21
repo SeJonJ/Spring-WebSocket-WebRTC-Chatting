e48d0328d3d1b7a0dc25d27919f50b999a2e23cd
sejon <wkdtpwhs@gmail.com>
2023-09-21 20:29:39 +0900

    긴급수정 :: 파일 업로드 기능 제거(S3 key 관련 이슈)

--- src/main/java/webChat/controller/FileController.java
+++ src/main/java/webChat/controller/FileController.java
@@ -24,11 +24,11 @@ public class FileController {
     @PostMapping("/upload")
     public FileUploadDto uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("roomId")String roomId){
 
-        FileUploadDto fileReq = fileService.uploadFile(file, UUID.randomUUID().toString(), roomId);
-        log.info("최종 upload Data {}", fileReq);
+//        FileUploadDto fileReq = fileService.uploadFile(file, UUID.randomUUID().toString(), roomId);
+//        log.info("최종 upload Data {}", fileReq);
 
         // fileReq 객체 리턴
-        return fileReq;
+        return null;
     }
 
     // get 으로 요청이 오면 아래 download 메서드를 실행한다.
@@ -36,12 +36,13 @@ public class FileController {
     @GetMapping("/download/{fileName}")
     public ResponseEntity<byte[]> download(@PathVariable String fileName, @RequestParam("fileDir")String fileDir){
         log.info("fileDir : fileName [{} : {}]", fileDir, fileName);
-        try {
-            // 변환된 byte, httpHeader 와 HttpStatus 가 포함된 ResponseEntity 객체를 return 한다.
-            return fileService.getObject(fileDir, fileName);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            // 변환된 byte, httpHeader 와 HttpStatus 가 포함된 ResponseEntity 객체를 return 한다.
+//            return fileService.getObject(fileDir, fileName);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        return null;
     }
 
 }
